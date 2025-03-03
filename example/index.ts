00a84bd0ad9e6b085a31452d2fc7db375f401f0b
nichoth <nichoth@nichoth.com>
2025-03-03 12:10:00 -0800

    rm unnecessary dependencies

--- example/index.ts
+++ example/index.ts
@@ -1,18 +1,7 @@
-import { type FunctionComponent, render } from 'preact'
-import { html } from 'htm/preact'
-import Debug from '@substrate-system/debug'
 import { report } from '../src/index.js'
-const debug = Debug()
 
-const data = report()
-debug('the report', data)
-
-const Example:FunctionComponent<unknown> = function () {
-    return html`<div>
-        <pre>
-            ${JSON.stringify(data, null, 2)}
-        </pre>
-    </div>`
-}
-
-render(html`<${Example} />`, document.getElementById('root')!)
+document.body.innerHTML = `
+<pre>
+    ${JSON.stringify(report(), null, 2)}
+</pre>
+`
